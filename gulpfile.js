d625e0dadf6675323c8b268c812b9c4b6e77a242
NPirotte <n.pirotte@synchrone.be>
2015-07-17 14:23:02 +0200

    Ajout des css du plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var less = require('gulp-less');
+var minifyCss = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('default', function()
+gulp.task('default', ['js', 'less']);
+
+gulp.task('js', function()
 {
   gulp.src('./src/*.js')
     .pipe(gulp.dest('./dist'))
@@ -11,4 +16,18 @@ gulp.task('default', function()
        extname: '.min.js'
      }))
     .pipe(gulp.dest('./dist'))
+});
+
+gulp.task('less', function()
+{
+  gulp
+    .src("./src/MobileSideNav.less")
+    .pipe(less())
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('./dist'))
+    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(rename({
+       extname: '.min.css'
+     }))
+    .pipe(gulp.dest('./dist'));
 });
